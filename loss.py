Fix loss calculation, change uspampling method

loss.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 import numpy as np
 import time
 from cv2.cv2 import resize
@@ -60,7 +61,7 @@ class VideoSaliencyLoss(nn.Module):
         loss_sim = self.similarity(pred, gt).cpu()
         loss_nss = self.nss(pred, fix).cpu()
         loss = torch.FloatTensor([0.0]).cpu()
-        loss += loss_sim + loss_nss
+        loss += loss_sim - loss_nss
         return loss, loss_sim, loss_nss
 
     def similarity(self, pred, gt):

model.py
@@ -396,7 +396,7 @@ class Mixed5c(nn.Module):
 class DecoderConv(nn.Module):
     def __init__(self):
         super(DecoderConv, self).__init__()
-        self.upsampling = nn.Upsample(scale_factor=(1, 2, 2))
+        self.upsampling = nn.Upsample(scale_factor=(1, 2, 2), mode='trilinear')
 
         self.conv1 = nn.Sequential(
             nn.Conv3d(1024, 832, kernel_size=(1, 3, 3), stride=(1, 1, 1), padding=(0, 1, 1), bias=False),